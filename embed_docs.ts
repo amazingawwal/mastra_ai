6df594bb00b705f264514699a85657c30ddb0c09
amazingawwal <sakeebmawwal@gmail.com>
2026-01-14 23:26:40 +0000

    feat:add embedding

--- embed_docs.ts
+++ embed_docs.ts
@@ -1,8 +1,9 @@
 import { Mastra } from "@mastra/core/mastra";
-import { embedMany } from "ai";
+import { embed, embedMany } from "ai";
 import { openai } from "@ai-sdk/openai";
 import { PgVector } from "@mastra/pg";
 import { MDocument } from "@mastra/rag";
+
 
 const documentText = `
 Mastra is a framework for building AI agents.
@@ -13,24 +14,28 @@ It supports memory, tools, workflows, and observability.
 const docs = MDocument.fromText(documentText)
 
 
-async function runChunk(){
-  const chunk = await docs.chunk({
+async function rag(){
+  const chunks = await docs.chunk({
   strategy:"recursive",
   maxSize:512,
-  overlap:50
-})
+  overlap:50 })
 
-console.log(chunk)
+ console.log(chunks)
+
+ const { embeddings } = await embedMany({
+  values: chunks.map((chunk) => chunk.text),
+  model: openai.embedding("text-embedding-3-small") ,
+ });
+
+ console.log("This is embedding:",embeddings)
 }
 
-runChunk();
+
+
+rag();
 
 
 
-// embeddings: new OpenAIEmbeddingModel({
-//     apiKey: process.env.OPENAI_API_KEY,
-//     model: "text-embedding-3-small",
-// });
 
 
 
