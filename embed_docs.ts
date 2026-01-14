0c9e6f3c1bf3151d36495b8d5fe12b8bc897422e
amazingawwal <sakeebmawwal@gmail.com>
2026-01-14 22:30:13 +0000

    feat:add chucking function

--- embed_docs.ts
+++ embed_docs.ts
@@ -4,49 +4,62 @@ import { openai } from "@ai-sdk/openai";
 import { PgVector } from "@mastra/pg";
 import { MDocument } from "@mastra/rag";
 
-
-
-
-
-
-
-
-  embeddings: new OpenAIEmbeddingModel({
-    apiKey: process.env.OPENAI_API_KEY,
-    model: "text-embedding-3-small",
-  });
-
-
 const documentText = `
 Mastra is a framework for building AI agents.
 It supports memory, tools, workflows, and observability.
 `;
 
 
-const splitter = new RecursiveTextSplitter({
-  chunkSize: 500,
-  chunkOverlap: 50,
-});
-
-const chunks2 = await splitter.splitText(documentText);
-
-console.log(chunks2);
+const docs = MDocument.fromText(documentText)
 
 
-const doc = MDocument.fromText(`Mastra is a framework for building AI agents.
-It supports memory, tools, workflows, and observability.
-`);
+async function runChunk(){
+  const chunk = await docs.chunk({
+  strategy:"recursive",
+  maxSize:512,
+  overlap:50
+})
 
-const chunks = await doc.chunk({
-  strategy: "recursive",
-  size: 512,
-  overlap: 50,
-});
+console.log(chunk)
+}
 
-console.log(chunks);
+runChunk();
 
-const embeddings = await mastra.embeddings.embedDocuments(chunks);
 
-console.log(embeddings);
 
-console.log(embeddings[0].embedding.length); // vector size
+// embeddings: new OpenAIEmbeddingModel({
+//     apiKey: process.env.OPENAI_API_KEY,
+//     model: "text-embedding-3-small",
+// });
+
+
+
+
+
+// const splitter = new RecursiveTextSplitter({
+//   chunkSize: 500,
+//   chunkOverlap: 50,
+// });
+
+// const chunks2 = await splitter.splitText(documentText);
+
+// console.log(chunks2);
+
+
+// const doc = MDocument.fromText(`Mastra is a framework for building AI agents.
+// It supports memory, tools, workflows, and observability.
+// `);
+
+// const chunks = await doc.chunk({
+//   strategy: "recursive",
+//   size: 512,
+//   overlap: 50,
+// });
+
+// console.log(chunks);
+
+// const embeddings = await mastra.embeddings.embedDocuments(chunks);
+
+// console.log(embeddings);
+
+// console.log(embeddings[0].embedding.length); // vector size
